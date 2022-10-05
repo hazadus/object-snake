46d331791bbd62d30e583754708e696ec5db9fc1
hazadus <hazadus7@gmail.com>
2022-10-05 18:28:07 +0300

    Shows game window.

--- object-snake.py
+++ object-snake.py
@@ -1,3 +1,6 @@
+import pygame
+
+
 class SnakeBlock:
     # x
     # y
@@ -24,11 +27,48 @@ class Food:
     pass
 
 
-class Board:
-    # width
-    # height
-    # score
-    # level (increase speed)
+class Game:
+    is_gameover = False
+    score = 0
+    level = 1
+
     # score_up()
     # hiscore, or high scores table
-    pass
+    def __init__(self, board_width: int, board_height: int):
+        """
+        :param board_width: ширина игрового поля (в блоках)
+        :param board_height: высота игрового поля (в блоках)
+        """
+        self.board_width = board_width
+        self.board_height = board_height
+
+    def game_over(self):
+        self.is_gameover = True
+
+
+class Engine:
+    def __init__(self, window_width: int, window_height: int, window_caption: str):
+        pygame.init()
+        self.display = pygame.display.set_mode((window_width, window_height))
+        pygame.display.set_caption(window_caption)
+
+    def quit(self):
+        pygame.quit()
+        quit()
+
+    def update(self):
+        pygame.display.update()
+
+
+if __name__ == '__main__':
+    engine = Engine(600, 600, 'Object Snake')
+    game = Game(60, 60)
+
+    while not game.is_gameover:
+        for event in pygame.event.get():
+            print(event)
+            if event.type == pygame.QUIT:
+                game.game_over()
+
+    engine.update()
+    engine.quit()

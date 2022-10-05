1b4a081a64290588c31a226fbabe3015339f7de2
hazadus <hazadus7@gmail.com>
2022-10-05 18:12:25 +0300

    Initial commit

---
+++ object-snake.py
@@ -0,0 +1,34 @@
+class SnakeBlock:
+    # x
+    # y
+    # is_head
+    pass
+
+
+class Snake:
+    # length
+    # blocks list
+    # head
+    # speed
+    # direction
+    #
+    # move()
+    # change_direction()
+    # grow()
+    pass
+
+
+class Food:
+    # x
+    # y
+    pass
+
+
+class Board:
+    # width
+    # height
+    # score
+    # level (increase speed)
+    # score_up()
+    # hiscore, or high scores table
+    pass

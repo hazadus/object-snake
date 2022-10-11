4d47430f4e2b153e9edefcb479924c534dac14dd
hazadus <hazadus7@gmail.com>
2022-10-11 11:07:52 +0300

    Step 6 of massive code refactoring - using encapsulation design pattern.

--- highscores.py
+++ highscores.py
@@ -4,36 +4,75 @@ import pickle
 
 
 class Score:
+    """
+    Описывает одну запись в таблице рекордов.
+
+    __name: имя игрока
+    __score: счет игры
+    __date_added: дата и время записи
+
+    Args:
+        name (str): имя игрока
+        score (int): счет игры
+        date_added (datetime): дата и время записи
+
+    """
     def __init__(self, name: str, score: int, date_added=datetime.now()):
         self.__name = name
         self.__score = score
-        self.date_added = date_added
+        self.__date_added = date_added
 
-    def score(self):
+    def score(self) -> int:
+        """
+        Значение счета в данной записи.
+
+        :return: __score
+        """
         return self.__score
 
+    def set_date_added(self, new_datetime: datetime):
+        """
+        Установить дату добавления записи в соответствии с переданной.
+
+        :param new_datetime: дата и время для установки в записи
+        """
+        self.__date_added = new_datetime
+
     def __repr__(self):
-        return f'{self.__name} {self.__score} {self.date_added.strftime("%d.%m.%Y %H:%M")}'
+        return f'{self.__name} {self.__score} {self.__date_added.strftime("%d.%m.%Y %H:%M")}'
 
 
 class Highscores:
     """
+    Описывает таблицу рекордов, в которой хранятся записи обо всех результатах игр. Сохраняется в объекте pickle.
     Данные из файла загружаются при создании инстанса!
     """
-    __highscores_filename = 'highscores.pkl'
-    __scores = list()
-
     def __init__(self):
+        self.__highscores_filename = 'highscores.pkl'
+        self.__scores = list()
         self.load()
 
     def add(self, new_score: Score):
-        new_score.date_added = datetime.now()
+        """
+        Добавляет новую запись о результатах игры в список.
+
+        :param new_score: запись для добавления
+        """
+        new_score.set_date_added(datetime.now())
         self.__scores.append(new_score)
 
-    def get(self):
+    def get_all(self):
+        """
+        Возвращает отсортированный по убыванию полный список записей о результатах игр.
+
+        :return: отсортированный по убыванию полный список записей о результатах игр состоящих из инстансов Score
+        """
         return sorted(self.__scores, key=lambda x: x.score(), reverse=True)
 
     def load(self):
+        """
+        Загружает записи из файла pickle.
+        """
         try:
             with open(self.__highscores_filename, 'rb') as file:
                 self.__scores = pickle.load(file)
@@ -42,6 +81,9 @@ class Highscores:
             logging.info(f'File "{self.__highscores_filename}" not found.')
 
     def save(self):
+        """
+        Сохраняет записи в файле pickle.
+        """
         # noinspection PyBroadException
         try:
             with open(self.__highscores_filename, 'wb') as file:
@@ -52,6 +94,9 @@ class Highscores:
 
 
 if __name__ == '__main__':
+    """
+    При запуске данного модуля – вывести таблицу рекордов.
+    """
     hs = Highscores()
-    for i, i_score in enumerate(hs.get()):
+    for i, i_score in enumerate(hs.get_all()):
         print(f'{i + 1}. {i_score}')
